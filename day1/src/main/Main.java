d8a9cfec2ada4a83514e627e10206cffcb76aee9
m1lor <bastianloritz@gmail.com>
2023-12-01 12:10:15 +0100

    -

---
+++ day1/src/main/Main.java
@@ -0,0 +1,114 @@
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    private static final String allDigitsRegex = "one|two|three|four|five|six|seven|eight|nine|\\d";
+
+    private static final Map<String, Character> nameToDigit = Map.of(
+            "one", '1',
+            "two", '2',
+            "three", '3',
+            "four", '4',
+            "five", '5',
+            "six", '6',
+            "seven", '7',
+            "eight", '8',
+            "nine", '9'
+    );
+
+    public static void main(String[] args) {
+        System.out.print("Enter file path: ");
+        final Scanner commandLineScanner = new Scanner(System.in);
+        final File file = new File(commandLineScanner.nextLine());
+        commandLineScanner.close();
+        final Scanner fileScanner;
+
+        try {
+            fileScanner = new Scanner(file);
+        } catch (FileNotFoundException exception) {
+            System.err.println("Invalid file path.");
+            return;
+        }
+
+        int part1Sum = 0;
+        int part2Sum = 0;
+
+        String currentLine;
+
+        while (fileScanner.hasNext()) {
+            currentLine = fileScanner.nextLine();
+            part1Sum += Integer.parseInt(new String(new char[]{
+                    findFirstDigit(currentLine, false),
+                    findLastDigit(currentLine, false)
+            }));
+            part2Sum += Integer.parseInt(new String(new char[]{
+                    findFirstDigit(currentLine, true),
+                    findLastDigit(currentLine, true)
+            }));
+        }
+
+        fileScanner.close();
+        System.out.println("Part 1: " + part1Sum);
+        System.out.println("Part 2: " + part2Sum);
+    }
+
+    private static char findFirstDigit(String line, boolean includeDigitNames) {
+        if (!includeDigitNames) {
+            for (char character : line.toCharArray()) {
+                if (Character.isDigit(character)) {
+                    return character;
+                }
+            }
+        } else {
+            Matcher digitMatcher;
+
+            for (int i = 0; i <= line.length(); i++) {
+                digitMatcher = Pattern.compile(allDigitsRegex).matcher(line.substring(0, i));
+                if (!digitMatcher.find()) {
+                    continue;
+                }
+
+                if (digitMatcher.group().length() == 1) {
+                    return digitMatcher.group().charAt(0);
+                }
+
+                return nameToDigit.get(digitMatcher.group());
+            }
+        }
+        return '0';
+    }
+
+    private static char findLastDigit(String line, boolean includeDigitNames) {
+        if (!includeDigitNames) {
+            for (int i = line.length() - 1; i >= 0; i--) {
+                if (Character.isDigit(line.charAt(i))) {
+                    return line.charAt(i);
+                }
+            }
+        } else {
+            Matcher digitMatcher;
+
+            for (int i = line.length() - 1; i >= 0; i--) {
+                digitMatcher = Pattern.compile(allDigitsRegex).matcher(line.substring(i));
+                if (!digitMatcher.find()) {
+                    continue;
+                }
+
+                if (digitMatcher.group().length() == 1) {
+                    return digitMatcher.group().charAt(0);
+                }
+
+                return nameToDigit.get(digitMatcher.group());
+            }
+        }
+        return '0';
+    }
+}
